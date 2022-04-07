a4d40e2e963098ad7a3d9ce51488c2fc60b0f071
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-04-07 23:01:07 +0900

    userクラス作成

--- src/main/java/journey/log/User.java
+++ src/main/java/journey/log/User.java
@@ -1,46 +1,62 @@
 package journey.log;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+import javax.persistence.QueryHint;
+import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "user")
+@NamedQuery(name = "Users.findAll", query = "SELECT f FROM user f ORDER BY f.name", hints = @QueryHint(name = "org.hibernate.cacheable", value = "true"))
 public class User {
 	
-	int id;
-	String uuid;
-	String handleName;
-	String displayName;
-	String password;
-	String hashedPassword;
+	private String uuid;
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+	@JsonProperty("handle_name")
+	@Column(name="handle_name", length = 40, unique = true)
+	private String handleName;
+	
+	@JsonProperty("display_name")
+	@Column(name="display_name", length = 40)
+	private String displayName;
+	
+	private String password;
+	
+	public User() {}
+
 	public String getUuid() {
 		return uuid;
 	}
+
 	public void setUuid(String uuid) {
 		this.uuid = uuid;
 	}
+
 	public String getHandleName() {
 		return handleName;
 	}
+
 	public void setHandleName(String handleName) {
 		this.handleName = handleName;
 	}
+
 	public String getDisplayName() {
 		return displayName;
 	}
+
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 
 }
